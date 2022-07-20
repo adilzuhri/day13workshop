2a5a0d616818a902183c610a07aae41be8de58c2
Adil Zuhri <106576506+adilzuhri@users.noreply.github.com>
2022-07-20 12:07:08 +0800

    included database service

--- src/main/java/sg/edu/nus/iss/day13workshop/Day13workshopApplication.java
+++ src/main/java/sg/edu/nus/iss/day13workshop/Day13workshopApplication.java
@@ -1,13 +1,45 @@
 package sg.edu.nus.iss.day13workshop;
 
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import sg.edu.nus.iss.day13workshop.services.DatabaseService;
+
 @SpringBootApplication
-public class Day13workshopApplication {
+public class Day13workshopApplication implements ApplicationRunner {
+
+	@Autowired
+	DatabaseService dbSvc;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Day13workshopApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) {
+		if (args.containsOption("dataDir")) {
+			final String dataDir = args.getOptionValues("dataDir").get(0);
+
+			System.out.printf("Using %s as data diretory\n", dataDir);
+
+			}else{
+				dbSvc.setDataDir(new File("./data"));
+
+			}
+	}
+
+	public boolean save() {
+		return false;
+
+	}
+
+	public void read(String fileId) {
+		
 	}
 
 }
